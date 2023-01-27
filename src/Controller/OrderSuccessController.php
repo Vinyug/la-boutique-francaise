949950aadb0d640a41651ee553502f9729be1e05
Vinyug <vincentguyomarch89@gmail.com>
2023-01-27 17:50:31 +0100

    OrderSuccessController : update name controller, and twig

--- src/Controller/OrderSuccessController.php
+++ src/Controller/OrderSuccessController.php
@@ -8,7 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class OrderValidateController extends AbstractController
+class OrderSuccessController extends AbstractController
 {
     private $entityManager;
 
