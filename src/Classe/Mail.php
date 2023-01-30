af35bf5478d5a9107c60326e2b7614b593edd5cf
Vinyug <vincentguyomarch89@gmail.com>
2023-01-30 15:55:40 +0100

    Class Mail

---
+++ src/Classe/Mail.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Classe;
+
+use Mailjet\Client;
+use Mailjet\Resources;
+
+class Mail 
+{
+    private $api_key = "cleAPI";
+    private $api_key_secret = "cleSecreteApi";
+
+    public function send($to_email, $to_name, $subject, $content) 
+    {
+        $mj = new Client($this->api_key, $this->api_key_secret, true,['version' => 'v3.1']);
+        $body = [
+            'Messages' => [
+                [
+                    'From' => [
+                        'Email' => "mailAssociéMailJet",
+                        'Name' => "La Boutique Française"
+                    ],
+                    'To' => [
+                        [
+                            'Email' => $to_email,
+                            'Name' => $to_name
+                        ]
+                    ],
+                    'TemplateID' => 4545677,
+                    'TemplateLanguage' => true,
+                    'Subject' => "$subject",
+                    'Variables' => [
+                        'content' => $content,
+                    ]
+                ]
+            ]
+        ];
+        
+        $response = $mj->post(Resources::$Email, ['body' => $body]);
+        $response->success() && dd($response->getData());
+    }
+}
